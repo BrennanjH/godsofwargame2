eb1408f8a0411843137728d64828e9af25c1e19d
BrennanjH <brennanjhammond@gmail.com>
2022-04-14 14:42:54 -0400

    Create HeavyTrackMovementPlatform test

--- src/main/java/com/simplesoftwaresolutions/godsofwargame/units/HeavyTrackMovementPlatform.java
+++ src/main/java/com/simplesoftwaresolutions/godsofwargame/units/HeavyTrackMovementPlatform.java
@@ -5,6 +5,8 @@
  */
 package com.simplesoftwaresolutions.godsofwargame.units;
 
+import com.simplesoftwaresolutions.godsofwargame.Terrain.Terrain;
+import com.simplesoftwaresolutions.godsofwargame.Terrain.TerrainTypes;
 import com.simplesoftwaresolutions.godsofwargame.game.BoardManager;
 import com.simplesoftwaresolutions.godsofwargame.location.PositionalCord;
 
@@ -15,9 +17,35 @@ import com.simplesoftwaresolutions.godsofwargame.location.PositionalCord;
 
 public class HeavyTrackMovementPlatform extends AbstractMovementPlatform{
 
+    private TerrainTypes[] validTerrain = {TerrainTypes.PLAINS};
+
+    public HeavyTrackMovementPlatform() {
+        speed = 1;
+        health = 10;
+    }
+
     @Override
     protected void move(PositionalCord locationData, BoardManager boardManager) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PositionalCord moveTo;
+
+        for(int i = 0; i < speed; i++){
+            //get the next movement location
+            if(movePath.getStepList().isEmpty()){
+                break;
+            }
+            moveTo = movePath.nextLocation();
+            //If location is valid change unit location to that
+            if(validateMove(locationData, moveTo, boardManager)){
+                locationData.setX(moveTo.getX());
+                locationData.setY(moveTo.getY());
+            } else {
+                //Clear units route as it is not valid if the next path is invalid
+                movePath.clear();
+
+                break; //with the move being invalid movement should stop
+            }
+        }
+
     }
 
     @Override
@@ -30,5 +58,25 @@ public class HeavyTrackMovementPlatform extends AbstractMovementPlatform{
         return 300;
     }
 
+    /** A method that checks if a coordinate is a valid position for this movement platform
+     * @param checkCoord - the co-ordinate that is being checked
+     * @return - true if passed in value is a possible move location
+     */
+    private boolean validateMove(PositionalCord startCoord,PositionalCord checkCoord, BoardManager manager){
+        //Validate distance
+        if ( Math.abs(startCoord.getX() - checkCoord.getX()) <= 1 &&
+                Math.abs(startCoord.getY() - checkCoord.getY()) <= 1 ) {
+
+            //Validate Terrain
+            Terrain nextPosTerrain = manager.getTerrain(checkCoord.getX(), checkCoord.getY());
+            for (TerrainTypes t :
+                    validTerrain) {
+                if (t == nextPosTerrain.getType()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
